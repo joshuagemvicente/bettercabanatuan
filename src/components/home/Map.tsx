6ec685054493960bca82e294e96fe77d03420c57
joshuagemvicente <joshuagemvicente6@gmail.com>
2026-06-14 14:25:37 +0800

    refactor: update Map component styles for improved layering and responsiveness

--- src/components/home/Map.tsx
+++ src/components/home/Map.tsx
@@ -32,17 +32,21 @@ export default function Map() {
         <Heading level={2}>Visit City Hall</Heading>
         <p className="text-gray-600 mt-2 flex items-center justify-center gap-2 max-w-2xl mx-auto">
           <MapPin className="w-4 h-4 shrink-0" />
-          <Link className='hover:underline text-primary-600' to="https://maps.google.com/maps?vet=10CAAQoqAOahcKEwjY-rLh1YSVAxUAAAAAHQAAAAAQFw..i&sca_esv=9773526fc95a52cb&pvq=CgwvZy8xaGMydGh6Y3I&fvr=1&cs=1&um=1&ie=UTF-8&fb=1&gl=ph&sa=X&ftid=0x339728f23e83546d:0xfa3d267b6c7aa1c1">
-          {cityHallLocation.address}
+          <Link
+            className="hover:underline text-primary-600"
+            to="https://maps.google.com/maps?vet=10CAAQoqAOahcKEwjY-rLh1YSVAxUAAAAAHQAAAAAQFw..i&sca_esv=9773526fc95a52cb&pvq=CgwvZy8xaGMydGh6Y3I&fvr=1&cs=1&um=1&ie=UTF-8&fb=1&gl=ph&sa=X&ftid=0x339728f23e83546d:0xfa3d267b6c7aa1c1"
+          >
+            {cityHallLocation.address}
           </Link>
         </p>
       </div>
 
-      <div className="rounded-xl overflow-hidden border border-gray-200 shadow-sm [&_.leaflet-pane]:z-[1]">
+      <div className="relative isolate z-0 rounded-xl overflow-hidden border border-gray-200 shadow-sm">
         <MapContainer
           center={position}
           zoom={cityHallLocation.zoom}
           scrollWheelZoom={false}
+          className="relative z-0"
           style={{ height: '400px', width: '100%' }}
         >
           <TileLayer
